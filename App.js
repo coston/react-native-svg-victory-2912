d34ec117fbbf72b4bacbebae12556a40d3e71b5b
Coston Perkins <costonperkins@me.com>
2024-11-19 14:59:09 -0600

    chore: init

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, SafeAreaView, Text as RNText } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+import { Text, Svg } from "react-native-svg";
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      Roboto: require("./assets/fonts/Roboto-Regular.ttf"),
+    });
+    setFontsLoaded(true);
+  };
+
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <RNText style={{ fontFamily: "Roboto" }}>
+        react-native: キロ, メートル coston
+      </RNText>
+
+      <Svg height="100" width="300">
+        <Text x="0" y="40" fontFamily="Roboto">
+          react-native-svg: キロ, メートル coston
+        </Text>
+      </Svg>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    margin: 16,
+  },
+  paragraph: {
+    margin: 8,
+    fontSize: 16,
+    textAlign: "center",
   },
 });
